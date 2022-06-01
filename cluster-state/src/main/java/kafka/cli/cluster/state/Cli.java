0fde879d727f0563109eaca0e16cb050fa4e1787
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-06-01 13:54:25 +0100

    refactor: reformat and place on new class path

---
+++ cluster-state/src/main/java/kafka/cli/cluster/state/Cli.java
@@ -0,0 +1,184 @@
+package kafka.cli.cluster.state;
+
+import static java.lang.System.err;
+import static java.lang.System.out;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import kafka.cli.cluster.state.Cli.VersionProviderWithConfigProvider;
+import kafka.context.KafkaContexts;
+import kafka.context.SchemaRegistryContexts;
+import org.apache.kafka.clients.admin.AdminClient;
+import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
+import picocli.CommandLine.Option;
+
+@Command(
+  name = "kfk-cluster-state",
+  descriptionHeading = "Kafka CLI - Topic list",
+  description = """
+                        List Kafka topics with metadata, partitions, replica placement, configuration,
+                         and offsets at once.
+                        """,
+  versionProvider = VersionProviderWithConfigProvider.class,
+  mixinStandardHelpOptions = true
+)
+public class Cli implements Callable<Integer> {
+
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new Cli()).execute(args);
+    System.exit(exitCode);
+  }
+
+  @Option(
+    names = { "-t", "--topics" },
+    description = "list of topic names to include"
+  )
+  List<String> topics = new ArrayList<>();
+
+  @Option(names = { "-p", "--prefix" }, description = "Topic name prefix")
+  Optional<String> prefix = Optional.empty();
+
+  @ArgGroup(multiplicity = "1")
+  PropertiesOption propertiesOption;
+
+  @Option(
+    names = { "--pretty" },
+    defaultValue = "false",
+    description = "Print pretty/formatted JSON"
+  )
+  boolean pretty;
+
+  @Override
+  public Integer call() throws Exception {
+    final var opts = new Opts(topics, prefix);
+
+    final var clientConfig = propertiesOption.load();
+
+    try (var adminClient = AdminClient.create(clientConfig)) {
+      final var helper = new Helper(adminClient);
+      final var output = helper.run(opts);
+      out.println(output.toJson(pretty));
+    }
+    return 0;
+  }
+
+  record Opts(List<String> topics, Optional<String> prefix) {
+    public boolean match(String name) {
+      return topics.contains(name) || prefix.map(name::startsWith).orElse(true);
+    }
+  }
+
+  static class PropertiesOption {
+
+    @CommandLine.Option(
+      names = { "-c", "--config" },
+      description = "Client configuration properties file." +
+      "Must include connection to Kafka"
+    )
+    Optional<Path> configPath;
+
+    @ArgGroup(exclusive = false)
+    ContextOption contextOption;
+
+    public Properties load() {
+      return configPath
+        .map(path -> {
+          try {
+            final var p = new Properties();
+            p.load(Files.newInputStream(path));
+            return p;
+          } catch (Exception e) {
+            throw new IllegalArgumentException(
+              "ERROR: properties file at %s is failing to load".formatted(path)
+            );
+          }
+        })
+        .orElseGet(() -> {
+          try {
+            return contextOption.load();
+          } catch (IOException e) {
+            throw new IllegalArgumentException("ERROR: loading contexts");
+          }
+        });
+    }
+  }
+
+  static class ContextOption {
+
+    @Option(
+      names = "--kafka",
+      description = "Kafka context name",
+      required = true
+    )
+    String kafkaContextName;
+
+    @Option(names = "--sr", description = "Schema Registry context name")
+    Optional<String> srContextName;
+
+    public Properties load() throws IOException {
+      final var kafkas = KafkaContexts.load();
+      final var props = new Properties();
+      if (kafkas.has(kafkaContextName)) {
+        final var kafka = kafkas.get(kafkaContextName);
+        final var kafkaProps = kafka.properties();
+        props.putAll(kafkaProps);
+
+        if (srContextName.isPresent()) {
+          final var srs = SchemaRegistryContexts.load();
+          final var srName = srContextName.get();
+          if (srs.has(srName)) {
+            final var sr = srs.get(srName);
+            final var srProps = sr.properties();
+            props.putAll(srProps);
+          } else {
+            err.printf(
+              "WARN: Schema Registry context `%s` not found. Proceeding without it.%n",
+              srName
+            );
+          }
+        }
+
+        return props;
+      } else {
+        err.printf(
+          "ERROR: Kafka context `%s` not found. Check that context already exist.%n",
+          kafkaContextName
+        );
+        return null;
+      }
+    }
+  }
+
+  static class VersionProviderWithConfigProvider implements IVersionProvider {
+
+    @Override
+    public String[] getVersion() throws IOException {
+      final var url =
+        VersionProviderWithConfigProvider.class.getClassLoader()
+          .getResource("cli.properties");
+      if (url == null) {
+        return new String[] {
+          "No cli.properties file found in the classpath.",
+        };
+      }
+      final var properties = new Properties();
+      properties.load(url.openStream());
+      return new String[] {
+        properties.getProperty("appName") +
+        " version " +
+        properties.getProperty("appVersion") +
+        "",
+        "Built: " + properties.getProperty("appBuildTime"),
+      };
+    }
+  }
+}
--- cluster-state/src/main/java/kafka/cli/cluster/state/Helper.java
+++ cluster-state/src/main/java/kafka/cli/cluster/state/Helper.java
@@ -1,10 +1,10 @@
-package kafka.cli.topics.list;
+package kafka.cli.cluster.state;
 
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import kafka.cli.topics.list.Cli.Opts;
+import kafka.cli.cluster.state.Cli.Opts;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.ListTopicsOptions;
 import org.apache.kafka.clients.admin.OffsetSpec;
@@ -19,8 +19,12 @@ public final class Helper {
     this.adminClient = adminClient;
   }
 
-  List<String> filterTopics(Opts opts) throws InterruptedException, ExecutionException {
-    final var all = adminClient.listTopics(new ListTopicsOptions()).listings().get();
+  List<String> filterTopics(Opts opts)
+    throws InterruptedException, ExecutionException {
+    final var all = adminClient
+      .listTopics(new ListTopicsOptions())
+      .listings()
+      .get();
     final var names = all.stream().map(TopicListing::name).toList();
     var filtered = names.stream().filter(opts::match).toList();
     return filtered.isEmpty() ? names : filtered;
@@ -32,36 +36,49 @@ public final class Helper {
     final var builder = Output.newBuilder(topics);
     final var describeClusterResult = adminClient.describeCluster();
 
-    final var descriptions = adminClient.describeTopics(builder.names()).allTopicNames().get();
+    final var descriptions = adminClient
+      .describeTopics(builder.names())
+      .allTopicNames()
+      .get();
 
     final var startOffsetRequest = new HashMap<TopicPartition, OffsetSpec>();
     final var endOffsetRequest = new HashMap<TopicPartition, OffsetSpec>();
 
     for (final var topic : builder.names) {
       final var description = descriptions.get(topic);
-      final var tps =
-          description.partitions().stream()
-              .map(tpi -> new TopicPartition(topic, tpi.partition()))
-              .sorted(Comparator.comparingInt(TopicPartition::partition))
-              .toList();
+      final var tps = description
+        .partitions()
+        .stream()
+        .map(tpi -> new TopicPartition(topic, tpi.partition()))
+        .sorted(Comparator.comparingInt(TopicPartition::partition))
+        .toList();
       for (final var tp : tps) {
         startOffsetRequest.put(tp, OffsetSpec.earliest());
         endOffsetRequest.put(tp, OffsetSpec.latest());
       }
     }
 
-    final var startOffsets = adminClient.listOffsets(startOffsetRequest).all().get();
-    final var endOffsets = adminClient.listOffsets(endOffsetRequest).all().get();
+    final var startOffsets = adminClient
+      .listOffsets(startOffsetRequest)
+      .all()
+      .get();
+    final var endOffsets = adminClient
+      .listOffsets(endOffsetRequest)
+      .all()
+      .get();
 
-    final var configs = adminClient.describeConfigs(builder.configResources()).all().get();
+    final var configs = adminClient
+      .describeConfigs(builder.configResources())
+      .all()
+      .get();
 
     return builder
-        .withClusterId(describeClusterResult.clusterId().get())
-        .withBrokers(describeClusterResult.nodes().get())
-        .withTopicDescriptions(descriptions)
-        .withStartOffsets(startOffsets)
-        .withEndOffsets(endOffsets)
-        .withConfigs(configs)
-        .build();
+      .withClusterId(describeClusterResult.clusterId().get())
+      .withBrokers(describeClusterResult.nodes().get())
+      .withTopicDescriptions(descriptions)
+      .withStartOffsets(startOffsets)
+      .withEndOffsets(endOffsets)
+      .withConfigs(configs)
+      .build();
   }
 }
